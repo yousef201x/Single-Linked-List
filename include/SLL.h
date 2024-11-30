67f568b71137ffe4a0b000bd57693bcecaee3496
Yousef201x <yousefmohamed2006x@gmail.com>
2024-11-30 06:39:43 +0200

    add push_front() function to SLL

--- include/SLL.h
+++ include/SLL.h
@@ -23,6 +23,31 @@ public:
     bool empty() {
         return this->head == nullptr;
     }
+
+
+    /*
+     * push_front()
+     * Inserts a new element at the front of the singly linked list.
+     * @param value: The value to be inserted in the new node.
+     */
+    void push_front(Ty& value) {
+        // Case 1: List is empty
+        if (this->empty()) {
+            this->head = new Node<Ty>(value, nullptr); // Create the first node
+            return;
+        }
+
+        // Case 2: List already has elements
+        // Temporarily store the current head node
+        Node<Ty>* temp = this->head;
+
+        // Create a new node and make it the head
+        this->head = new Node<Ty>(value, nullptr);
+
+        // Link the new head node to the previously stored node
+        this->head->next = temp;
+    }
+
 };
 
 #endif // INC_1_SINGLE_LINKED_LIST_SLL_H
